0b3c5a0b7de8006025115c1e1fb1f2f6ee1a7525
cs22vaibhavi <cs22vaibhavi@svpkumta.in>
2025-03-04 11:00:54 +0530

    first commit

---
+++ myapp/src/Components/NaukriHeader.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./NaukriHeader.css";
+import {FaCaretDown} from 'react-icons/fa';
+import { FaSearch } from "react-icons/fa";
+
+
+const NaukriHeader = () => {
+  return (
+    <header className="naukri-header">
+      <div className="naukri-logo">
+        <img src="https://ts1.mm.bing.net/th?id=OIP.79135SDgGpsK7lsujRR9MAAAAA&pid=15.1" height="5px" width="100px"alt="Naukri Logo" />
+      </div>
+      <nav className="naukri-nav">
+        <a href="#">Jobs</a>
+        <a href="#">Companies</a>
+        <a href="#">Services</a>
+      </nav>
+      <div className="naukri-search">
+        <input type="text" placeholder="Search jobs here" />
+           <span className="icon"> <FaSearch /></span>     
+      </div>
+      <div className="naukri-auth">
+        <button className="login-btn">Login</button>
+        <button className="register-btn">Register</button>
+      </div>
+      <div className="naukri-employers">
+        <span>For employers </span>
+        
+        <FaCaretDown size={22} />
+      </div>
+    </header>
+  );
+};
+
+export default NaukriHeader;
---
+++ myapp/src/Components/NaukriHeader.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NaukriHeader from "./NaukriHeader";
+
+export default {
+  title: "Components/NaukriHeader",
+  component: NaukriHeader,
+};
+
+export const Default = () => <NaukriHeader />;
